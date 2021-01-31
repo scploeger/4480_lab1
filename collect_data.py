40cab79811fa61d79f3c3576d48dc5bb79f718b9
sploeger <spenceinvents@homtail.com>
2021-01-31 01:26:05 +0000

    updatred collect_data.py

--- collect_data.py
+++ collect_data.py
@@ -1,40 +1,47 @@
-#from: https://github.com/Intelligent-Vehicle-Perception/MPU-9250-Sensors-Data-Collect
+#By Spencer Ploeger
+#Updated: Jan/30/2021
+#ENGG*4480 Lab 1
+
+#IMU library from: https://github.com/Intelligent-Vehicle-Perception/MPU-9250-Sensors-Data-Collect
 import sys
 import os
 import matplotlib
-matplotlib.use('Agg') #use non-interactive backend
+matplotlib.use('Agg') #use non-interactive backend (needed for matplotlib to run on pi) #must be called before importing pyplot
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 sys.path.append("")
-
 import time
 from mpu9250_jmdev.registers import *
 from mpu9250_jmdev.mpu_9250 import MPU9250
 
-config = 0
-trial_num = "2"
 
-#mpu.configure() # Apply the settings to the registers.
+config = 0 #for acceleromater scale setting input check
+trial_num = "1" #for naming output files and labling graphs
 
-#empty array for ay
+
+#empty arrays for imu data
 a = []
 g = []
 m = []
 
-
+#Plot function
+#parmeters: none
+#returns: none
+#this function plots Acceleration and Gyroscope values
 def plot():
      #convert array of tuples to a numpy array
         a_data = np.array(a)
         g_data = np.array(g)
         m_data = np.array(m)
 
+        #export the accelerometer and gyro data to CSV files
         np.savetxt('Trial ' + trial_num + ' - Accelerometer Data.csv', a, delimiter=",")
         np.savetxt('Trial ' + trial_num + ' - Gyroscope Data.csv', g, delimiter=",")
 
         #plot A graph
         plt.figure()
-        plt.plot(a_data[:,0], label = "a_x")
+        plt.plot(a_data[:,0], label = "a_x") #plot the X, Y, Z data seperately
         plt.plot(a_data[:,1], label = "a_y")
         plt.plot(a_data[:,2], label = "a_z")
         plt.legend(loc="upper left")
@@ -46,7 +53,7 @@ def plot():
 
         #plot G graph
         plt.figure()
-        plt.plot(g_data[:,0], label = "g_x")
+        plt.plot(g_data[:,0], label = "g_x") #plot the X, Y, Z data seperately
         plt.plot(g_data[:,1], label = "g_y")
         plt.plot(g_data[:,2], label = "g_z")
         plt.legend(loc="upper left")
@@ -56,7 +63,7 @@ def plot():
         plt.title('Trial ' + trial_num + ' - Gyroscope Data')
         plt.savefig('Trial ' + trial_num + ' - Gyroscope Data.png')
 
-
+#main function
 def main(config):
 
     mpu = MPU9250(
@@ -64,7 +71,7 @@ def main(config):
     address_mpu_master=MPU9050_ADDRESS_68, # In 0x68 Address
     address_mpu_slave=None, 
     bus=1,
-    afs=AFS_2G,
+    afs=AFS_2G, #sensitivity defaults to 2G
     gfs=GFS_1000,
     mfs=AK8963_BIT_16, 
     mode=AK8963_MODE_C100HZ)
@@ -79,28 +86,28 @@ def main(config):
         if(val != '2' and val != '4' and val != '8' and val != '6'):
             print("Only enter 2, 4, 8, or 1!")
         else:
+            #set the desired accelerometer scaler
+            if val == '2':
+                setattr(mpu, 'afs', AFS_2G)
+            elif val == '4':
+                setattr(mpu, 'afs', AFS_4G)
+            elif val == '8':
+                setattr(mpu, 'afs', AFS_8G)
+            elif val == '6':
+                setattr(mpu, 'afs', AFS_16G)
+
             config = 1
 
-    if val == '2':
-        setattr(mpu, 'afs', AFS_2G)
-    elif val == '4':
-        setattr(mpu, 'afs', AFS_4G)
-        print("val is 4!!")
-    elif val == '8':
-        setattr(mpu, 'afs', AFS_8G)
-    elif val == '16':
-        setattr(mpu, 'afs', AFS_16G)
-
-    mpu.configure()
-    print(mpu.getAllSettings())
+    mpu.configure() #write settings to imu registers
+    print(mpu.getAllSettings()) #print settings to confirm they are correct
 
     try:
-        while(itr < num_samples):
-            a.append(mpu.readAccelerometerMaster())
+        while(itr < num_samples):#iterate until desired number of samples is reached
+            a.append(mpu.readAccelerometerMaster()) #append values read from IMU to respective array
             g.append(mpu.readGyroscopeMaster())
             m.append(mpu.readMagnetometerMaster())
 
-            print("Accelerometer", a[len(a)-1])
+            print("Accelerometer", a[len(a)-1]) #print the most recent values
             print("Gyroscope", g[len(g)-1])
             print("Magnetometer", g[len(g)-1])
             #print("Temperature", mpu.readTemperatureMaster())
@@ -109,11 +116,11 @@ def main(config):
             itr += 1
 
             time.sleep(0.1)
-        plot()
-    except KeyboardInterrupt:
-        plot()
+        plot() #after collecting data, plot charts and export CSV files
+    except KeyboardInterrupt: #if CTRL+C is pressed
+        plot() #...still plot
         print('Interrupted')
-        try:
+        try: #then exit
             sys.exit(0)
         except SystemExit:
             os._exit(0)
